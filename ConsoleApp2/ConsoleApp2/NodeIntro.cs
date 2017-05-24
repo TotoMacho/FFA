5a3f2a4f790adf3b9eb5d820ff823f3dde3d1859
TotoMacho <anthony.ouinet@viacesi.fr>
2017-05-24 12:40:41 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/NodeIntro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class NodeIntro
+    {
+        public NodeIntro()
+        {
+            String[] action = { "look", "lookaround", "move", "up", "down", "talk", "exit", "kill", "1", "2", "3", "4" };
+
+         
+        }
+        public void look(String nomObject)
+        {
+            /**getDescriptionObject(nomObject){
+
+            }**/
+        }
+        public void getDescriptionObject(String nObjet)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\" + nObjet + ".txt");
+            System.Console.WriteLine("{0}", text);
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NodeIntro myObject = new NodeIntro();
+
+           
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"c:\NodeIntro.txt");
+            while ((line = file.ReadLine()) != ";")
+            {
+                System.Console.WriteLine(line);
+               
+            }
+
+            file.Close();
+      
+            // Suspend the screen.  
+            System.Console.ReadLine();
+
+            System.Console.ReadKey();
+        }
+    }
+
+}
